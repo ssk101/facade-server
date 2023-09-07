c0932eed4980fbd66d7cbf24fb16bd537e88d510
steelskysoftware <steelskysoftware@gmail.com>
2023-09-07 09:44:14 +0200

    refactors

--- middlewares/serialize.js
+++ middlewares/serialize.js
@@ -21,7 +21,9 @@ export function serialize(opts, handler) {
     try {
       const response = await handler(req, res, next)
 
-      if(Buffer.isBuffer(response)) {
+      if(typeof response === 'undefined') {
+        // res.set('Content-Type', req.headers['content-type'])
+      } else if(Buffer.isBuffer(response)) {
         res.set('Content-Encoding', 'gzip')
         res.set('Content-Type', 'application/json')
         res.send(response)
